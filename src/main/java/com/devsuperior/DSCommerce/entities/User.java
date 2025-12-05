bf8ef5f66f45a529220f22f56bb30c622cd14539
unknown <deivid.cardoso.100@gmail.com>
2025-12-05 17:33:20 -0300

    conexao h2 db

---
+++ src/main/java/com/devsuperior/DSCommerce/entities/User.java
@@ -0,0 +1,68 @@
+package com.devsuperior.DSCommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+@Entity
+@Table(name = "tb_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long  id;
+    private String name;
+    private String email;
+    private LocalDate birthDate;
+    private String password;
+
+    public User() {
+
+    }
+
+    public User(Long id, String name, String email, LocalDate birthDate, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
