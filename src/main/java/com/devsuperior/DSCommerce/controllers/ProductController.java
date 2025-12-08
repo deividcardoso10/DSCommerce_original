ea5880a9ba9b89448eca629db1529242a411e11c
unknown <deivid.cardoso.100@gmail.com>
2025-12-08 12:50:30 -0300

    adicionando um novo produto

--- src/main/java/com/devsuperior/DSCommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/DSCommerce/controllers/ProductController.java
@@ -5,10 +5,7 @@ import com.devsuperior.DSCommerce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping (value = "/products")
@@ -23,9 +20,14 @@ public class ProductController {
         return dto;
     }
 
-    @GetMapping
+    @PostMapping
     public Page<ProductDTO> findAll(Pageable pageable){
         return service.findAll(pageable);
+
+    }
+    @GetMapping
+    public ProductDTO insert(@RequestBody ProductDTO dto){
+        return service.insert(dto);
 
     }
 
